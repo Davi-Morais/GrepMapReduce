b4202db649ff5f29c912485652276fd556a931e2
Davi Morais <davifarias2433@gmail.com>
2024-03-31 21:40:26 -0300

    implementando argumentos de linha de comando

--- grep.py
+++ grep.py
@@ -1,3 +1,5 @@
+import argparse
+from sys import exit
 from concurrent.futures import ThreadPoolExecutor
 from os import listdir
 
@@ -7,6 +9,24 @@ from emit import deletar_intermediario, deletar_final, ler_intermediario
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+                    prog='Grep MapReduce',
+                    description='Procura padroes de texto e gera um arquivo final com as ocorrencias',
+                    epilog='MapReduce Sistemas Distribuidos 2024')
+    parser.add_argument('padrao', nargs='?', help="Texto que vai ser buscado.")
+    parser.add_argument('-e', help="Recebe uma expressao regular.")
+
+    args = parser.parse_args()
+    
+    padrao_busca = None
+    if args.padrao:
+        padrao_busca = args.padrao
+    elif args.e:
+        padrao_busca = args.e
+    else:
+        print("Nenhum padrao informado...")
+        exit(1)
 
     intermadiario = './intermediario'
     final = './final'
@@ -22,7 +42,7 @@ if __name__ == "__main__":
     # Maximo de 10 threads:
     with ThreadPoolExecutor(max_workers=10) as executor:
         for index in range(quantidade_arquivos):
-            executor.submit(Map, f"arquivo{index}.txt", "foo")
+            executor.submit(Map, f"arquivo{index}.txt", padrao_busca)
 
 
 
